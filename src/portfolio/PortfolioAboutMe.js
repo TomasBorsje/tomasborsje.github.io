2d1dedea373f26e6c126bec481847734612dffe4
Tomas Borsje <borsjetoma@myvuw.ac.nz>
2025-01-28 18:15:02 +1300

    feat: Add DuskfalLServer and OMT-Reloaded portfolio entries

--- src/portfolio/PortfolioAboutMe.js
+++ src/portfolio/PortfolioAboutMe.js
@@ -8,7 +8,7 @@ class PortfolioAboutMe extends Component {
                 <div id="PortfolioEntryTextBody">
                     <div id="PortfolioEntryTitle">About Me</div>
                     <div id="PortfolioAboutMeDescription">Hey, I'm Tomas! I'm a 22 year old software engineer with a passion for coding. I enjoy working on various personal projects spanning across games in my spare time.</div>
-                    <div id="PortfolioAboutMeDescription">In 2024, I graduated from VUW with a Bachelors of Engineering (Honours) with first class honours. I've also been working as an RPA developer since 2021.</div>
+                    <div id="PortfolioAboutMeDescription">In 2024, I graduated from VUW with a Bachelors of Engineering (Honours) with first class honours, majoring in Software Engineering. I've also been working as an RPA developer since 2021.</div>
                     <div id="PortfolioAboutMeDescription">Below is a list of my personal projects. I recommend checking out Duskfall's showcase video if you'd like to see my Java/Spigot work. Thanks!</div>
                 </div>
             </div>
